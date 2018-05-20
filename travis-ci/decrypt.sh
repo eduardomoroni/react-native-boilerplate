a82d82fa65bfc175a928015705aec3e920bae72f
Eduardo Moroni <eduardomoroni@gmail.com>
2018-05-20 20:09:00 -0300

    ci: encrypt againg android google metadata

--- travis-ci/decrypt.sh
+++ travis-ci/decrypt.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
-openssl aes-256-cbc -K $encrypted_aff1b2d8f811_key -iv $encrypted_aff1b2d8f811_iv \
-  -in travis-ci/google-services.json.enc \
-  -out android/app/google-services.json -d
+echo "Decrypting google-services.json..."
+openssl aes-256-cbc -K $encrypted_aff1b2d8f811_key -iv $encrypted_aff1b2d8f811_iv -in google-services.json.enc -out android/app/google-services.json -d
 
+echo "Decrypting GoogleService-Info.plist..."
 openssl aes-256-cbc -K $encrypted_aff1b2d8f811_key -iv $encrypted_aff1b2d8f811_iv \
   -in travis-ci/GoogleService-Info.plist.enc \
   -out ios/GoogleService-Info.plist -d
 
+echo "Decrypting bitbucket ssh key..."
 openssl aes-256-cbc -K $encrypted_84b6efd66213_key -iv $encrypted_84b6efd66213_iv \
   -in travis-ci/bitbucket_key.enc \
   -out bitbucket_key -d
