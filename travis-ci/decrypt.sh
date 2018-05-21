c4bbeaead776b0172689dd043a43502876450d0b
Eduardo Moroni <eduardomoroni@gmail.com>
2018-05-21 06:33:53 -0300

    ci: using secrets repository instead of encrypted files

--- travis-ci/decrypt.sh
+++ travis-ci/decrypt.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env sh
 # https://docs.travis-ci.com/user/encrypting-files/
 
-echo "Decrypting google-services.json..."
-openssl aes-256-cbc -K $ENCRYPT_PASSWORD -iv $ENCRYPT_IV \
-  -in travis-ci/google-services.json.enc \
-  -out android/app/google-services.json -d
-
-echo "Decrypting GoogleService-Info.plist..."
-openssl aes-256-cbc -K $ENCRYPT_PASSWORD -iv $ENCRYPT_IV \
-  -in travis-ci/GoogleService-Info.plist.enc \
-  -out ios/GoogleService-Info.plist -d
-
 echo "Decrypting bitbucket ssh key..."
 openssl aes-256-cbc -K $encrypted_84b6efd66213_key \
   -iv $encrypted_84b6efd66213_iv \
@@ -20,3 +10,11 @@ openssl aes-256-cbc -K $encrypted_84b6efd66213_key \
 eval "$(ssh-agent)"
 chmod 600 bitbucket_key
 ssh-add bitbucket_key
+
+echo "Cloning secrets repository..."
+REPO_SSH_URL="git@bitbucket.org:eduardomoroni/trading-card-manager-secrets.git"
+SECRETS_PATH="trading-card-manager-secrets/secrets"
+git clone --depth 1 "$REPO_SSH_URL"
+mv "$SECRETS_PATH/google-services.json" android/app/google-services.json
+mv "$SECRETS_PATH/GoogleService-Info.plist" ios/GoogleService-Info.plist
+mv "$SECRETS_PATH/secrets/.env" .env
