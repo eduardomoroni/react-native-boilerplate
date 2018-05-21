021adfdda1a788cd53cf69951846709604cbe5cf
Eduardo Moroni <eduardomoroni@gmail.com>
2018-05-20 22:45:44 -0300

    ci: manually encrypting files

--- travis-ci/decrypt.sh
+++ travis-ci/decrypt.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env sh
+# https://docs.travis-ci.com/user/encrypting-files/
 
 echo "Decrypting google-services.json..."
-openssl aes-256-cbc -K $encrypted_aff1b2d8f811_key -iv $encrypted_aff1b2d8f811_iv -in travis-ci/google-services.json.enc -out android/app/google-services.json -d
+openssl aes-256-cbc -K $ENCRYPT_PASSWORD -iv $ENCRYPT_IV \
+  -in travis-ci/google-services.json.enc \
+  -out android/app/google-services.json -d
 
 echo "Decrypting GoogleService-Info.plist..."
-- openssl aes-256-cbc -K $encrypted_aff1b2d8f811_key -iv $encrypted_aff1b2d8f811_iv -in travis-ci/GoogleService-Info.plist.enc -out ios/GoogleService-Info.plist -d
+openssl aes-256-cbc -K $ENCRYPT_PASSWORD -iv $ENCRYPT_IV \
+  -in travis-ci/GoogleService-Info.plist.enc \
+  -out ios/GoogleService-Info.plist -d
 
 echo "Decrypting bitbucket ssh key..."
-openssl aes-256-cbc -K $encrypted_84b6efd66213_key -iv $encrypted_84b6efd66213_iv \
+openssl aes-256-cbc -K $encrypted_84b6efd66213_key \
+  -iv $encrypted_84b6efd66213_iv \
   -in travis-ci/bitbucket_key.enc \
   -out bitbucket_key -d
 
