f1da04adab7418985aba45bc60a7af1d68d92387
Eduardo Moroni <eduardomoroni@gmail.com>
2018-05-24 22:40:07 -0300

    chore: add clear-cache script

--- scripts/clear-cache.sh
+++ scripts/clear-cache.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
 watchman watch-del-all
-rm -rf node_modules
-npm install
+rm -rf ios/build
+rm -rf ios/Pod
+rm -rf android/build
 rm -rf /tmp/haste-map-react-native-packager-*
 rm -rf /tmp/metro-bundler-cache-*
+cd ios
